c3142e9b37f48a1925c09c10359ab5ed8899cf0b
Serik <elwynaps@gmail.com>
2021-04-12 17:02:14 +0600

    CustomTableViewCell

---
+++ Places/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Elwyn on 12.04.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    
+}
